01094ab02c2b1625c82d2a887c25ad072c3cb877
Rom Burla <romwebdev@gmail.com>
2022-07-09 20:50:52 +0300

    commit 2

--- app/app.js
+++ app/app.js
@@ -1,1 +1,4 @@
 let root = document.getElementById("root");
+
+root.innerHTML =
+  '<h1>The Big Bang</h1><img src="./img/BigBangTheoryTitleCard.png" alt="The big bang logo"><div id="name">?</div><button id="btnp">+</button><button id="btnm">-</button>';
